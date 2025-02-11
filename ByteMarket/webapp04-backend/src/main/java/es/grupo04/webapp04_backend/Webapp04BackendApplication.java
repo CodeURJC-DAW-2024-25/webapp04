31e162d79da0642104c2444aabbcf2e79b3ffdc6
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2025-02-11 10:20:59 +0100

    Created SpringBoot Maven project

---
+++ ByteMarket/webapp04-backend/src/main/java/es/grupo04/webapp04_backend/Webapp04BackendApplication.java
@@ -0,0 +1,13 @@
+package es.grupo04.webapp04_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp04BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp04BackendApplication.class, args);
+	}
+
+}
---
+++ ByteMarket/webapp04-backend/src/test/java/es/grupo04/webapp04_backend/Webapp04BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grupo04.webapp04_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp04BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
