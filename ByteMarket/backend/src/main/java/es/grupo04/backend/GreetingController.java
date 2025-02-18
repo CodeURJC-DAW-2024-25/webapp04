c8945ef0769777712af27f3621cf252c2900f5ed
chubi0l <130486125+chubi0l@users.noreply.github.com>
2025-02-18 12:03:55 +0100

    Icon shown solved

--- ByteMarket/backend/src/main/java/es/grupo04/backend/GreetingController.java
+++ ByteMarket/backend/src/main/java/es/grupo04/backend/GreetingController.java
@@ -1,14 +1,34 @@
 package es.grupo04.backend;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class GreetingController {
+
+    @ModelAttribute
+    public void addAttributes(Model model, HttpServletRequest request) {
+
+		Principal principal = request.getUserPrincipal();
+
+		if (principal != null) {
+
+			model.addAttribute("logged", true);
+			model.addAttribute("userName", principal.getName());
+			model.addAttribute("admin", request.isUserInRole("ADMIN"));
+
+		} else {
+			model.addAttribute("logged", false);
+		}
+	}
 
     @GetMapping("/")
     public String home(Model model) {
