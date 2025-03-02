b33ead5e8807336d95e038b9b87b2709fe800d28
adri04ms <mserrano.adrian@gmail.com>
2025-03-02 18:24:41 +0100

    mysql database

--- ByteMarket/backend/src/main/java/es/grupo04/backend/model/Review.java
+++ ByteMarket/backend/src/main/java/es/grupo04/backend/model/Review.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 @Entity
 public class Review {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(columnDefinition = "TEXT")
