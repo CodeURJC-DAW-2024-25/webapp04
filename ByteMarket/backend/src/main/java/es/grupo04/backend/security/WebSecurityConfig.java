3b137b4c1cd20c5cebe0959063223d4719082348
chubi0l <130486125+chubi0l@users.noreply.github.com>
2025-03-17 17:14:03 +0100

    Security uploaded

--- ByteMarket/backend/src/main/java/es/grupo04/backend/security/WebSecurityConfig.java
+++ ByteMarket/backend/src/main/java/es/grupo04/backend/security/WebSecurityConfig.java
@@ -3,16 +3,29 @@ package es.grupo04.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import es.grupo04.backend.security.jwt.JwtRequestFilter;
+import  es.grupo04.backend.security.jwt.UnauthorizedHandlerJwt;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
 
 	@Autowired
     public RepositoryUserDetailsService userDetailService;
@@ -33,6 +46,7 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
+	@Order(1)
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
 		http.authenticationProvider(authenticationProvider());
@@ -77,6 +91,44 @@ public class WebSecurityConfig {
 					.logoutSuccessUrl("/")
 					.permitAll()
 			);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authenticationProvider(authenticationProvider());
+		
+		http
+			.securityMatcher("/api/**")
+			.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+		
+		http
+			.authorizeHttpRequests(authorize -> authorize
+                    // PRIVATE ENDPOINTS
+                    .requestMatchers(HttpMethod.POST,"/api/products/").hasRole("USER")
+                    .requestMatchers(HttpMethod.PUT,"/api/products/**").hasRole("USER")
+                    .requestMatchers(HttpMethod.DELETE,"/api/products/**").hasRole("ADMIN")
+					// PUBLIC ENDPOINTS
+					.anyRequest().permitAll()
+			);
+		
+        // Disable Form login Authentication
+        http.formLogin(formLogin -> formLogin.disable());
+
+        // Disable CSRF protection (it is difficult to implement in REST APIs)
+        http.csrf(csrf -> csrf.disable());
+
+        // Disable Basic Authentication
+        http.httpBasic(httpBasic -> httpBasic.disable());
+
+        // Stateless session
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Add JWT Token filter
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
