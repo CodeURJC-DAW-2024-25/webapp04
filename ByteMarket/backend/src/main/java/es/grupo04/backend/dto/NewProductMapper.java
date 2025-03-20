86f7bf8be135c9f24506720e8ca77b08c4580a6c
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2025-03-20 10:12:20 +0100

    docker scripts for creating and publishing the webapp image

--- ByteMarket/backend/src/main/java/es/grupo04/backend/dto/NewProductMapper.java
+++ ByteMarket/backend/src/main/java/es/grupo04/backend/dto/NewProductMapper.java
@@ -1,12 +1,23 @@
 package es.grupo04.backend.dto;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import es.grupo04.backend.model.Product;
 
 @Mapper(componentModel = "spring")
 public interface NewProductMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "favoriteOfUsers", ignore = true)
+    @Mapping(target = "imageFile", ignore = true)
+    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "publishDate", ignore = true)
+    @Mapping(target = "reports", ignore = true)
+    @Mapping(target = "sold", ignore = true)
+    @Mapping(target = "thumbnail", ignore = true)
+    @Mapping(target = "users", ignore = true)
     Product toDomain(NewProductDTO newProductDTO);
 
 }
---
+++ Docker/create_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Image name and tag
+IMAGE_NAME="webapp04"
+IMAGE_TAG="latest"
+
+#build image
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f ./Dockerfile ../ByteMarket/backend
