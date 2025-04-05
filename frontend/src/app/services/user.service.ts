9a9ed3766289aebaa2271df47ee61f4b1d594796
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2025-04-05 17:52:32 +0200

    added TODO

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -10,8 +10,9 @@ import { UserBasicDTO } from '../dtos/user.basic.dto';
 export class UserService {
     constructor(private http: HttpClient) { }
     
+    //TODO: Poner un endpoint para las imagenes en la API
     getUser(): Observable<UserBasicDTO> {
-        let url = '/api/v1/users/2';
+        let url = '/api/v1/users/me';
         return this.http.get<UserBasicDTO>(url);
     }
 }
