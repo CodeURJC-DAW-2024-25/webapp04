6236d4e3eaee203f0aefbdacad6fac57513e39bd
chubi0l <130486125+chubi0l@users.noreply.github.com>
2025-04-24 20:19:31 +0200

    Fix minor detail in Docker files

--- Docker/create_image.sh
+++ Docker/create_image.sh
@@ -5,4 +5,4 @@ IMAGE_NAME="webapp04"
 IMAGE_TAG="latest"
 
 #build image
-docker build -t $IMAGE_NAME:$IMAGE_TAG -f ./Dockerfile ../ByteMarket/backend
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f Dockerfile ..
