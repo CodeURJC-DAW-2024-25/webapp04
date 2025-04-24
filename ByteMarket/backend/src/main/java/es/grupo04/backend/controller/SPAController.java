b6ad3724870216dfa3b906bc3b7346e26341515f
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2025-04-25 00:34:54 +0200

    fixed Dockerfile and Docker Compose to make them work correctly, added needed configuration for the SPA to work

---
+++ ByteMarket/backend/src/main/java/es/grupo04/backend/controller/SPAController.java
@@ -0,0 +1,16 @@
+package es.grupo04.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SPAController {
+
+    @GetMapping({"/spa", "/spa/**/{path:[^\\.]*}", "/{path:spa[^\\.]*}"})
+    public String redirect() {
+
+        return "forward:/spa/index.html";
+
+    }
+
+}
--- Docker/create_image.sh
+++ Docker/create_image.sh
@@ -5,4 +5,4 @@ IMAGE_NAME="webapp04"
 IMAGE_TAG="latest"
 
 #build image
-docker build -t $IMAGE_NAME:$IMAGE_TAG -f Dockerfile ..
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f ./Dockerfile ..
